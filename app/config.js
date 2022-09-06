1c2e20795fe84eda83a683f74d5e49d67a094885
Rob Walpole <2248695+rwalpole@users.noreply.github.com>
2022-09-06 18:59:16 +0100

    Further development

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Catalogue Editorial Interface',
+  serviceName: 'Pan-Archival Catalogue',
 
   // Default port that prototype runs on
   port: '3000',
