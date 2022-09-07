49924c68bccb2cee561d19d12fc72fcd6e1af35f
Rob Walpole <2248695+rwalpole@users.noreply.github.com>
2022-09-07 18:19:36 +0100

    Further development

--- app/routes.js
+++ app/routes.js
@@ -19,4 +19,21 @@ router.post('/juggling-balls-answer', function (req, res) {
 
 })
 
+router.post('/login-post', function (req, res) {
+
+  // Make a variable and give it the value from 'how-many-balls'
+  var username = req.session.data['username']
+  var password = req.session.data['password']
+
+  // Check whether the variable matches a condition
+  if (username != '' && password != ''){
+    // Send user to next page
+    res.redirect('/edit-set')
+  } else {
+    // Send user to ineligible page
+    res.redirect('/login-invalid')
+  }
+
+})
+
 module.exports = router
